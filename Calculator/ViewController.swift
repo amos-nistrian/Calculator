1c188b0e42d58cf1f0f85c07b2ec4bb7f21e8ffa
amosnistrian <amosnistrian@gmail.com>
2017-07-18 14:05:40 -0700

    finished ui

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -8,8 +8,25 @@
 
 import UIKit
 
+enum modes {
+    case ADDITION
+    case SUBTRACTION
+    case MULTIPLICATION
+    case DIVISION
+    //case NEGATIVE
+    case NO_MODE_SET
+}
+
 class ViewController: UIViewController {
 
+    @IBOutlet weak var label: UILabel!
+    var score:Int = 0
+    
+    var labelString:String = "0"
+    var currentMode:modes = modes.NO_MODE_SET
+    var savedNum:Int = 0
+    var lastButtonWasMode:Bool = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
