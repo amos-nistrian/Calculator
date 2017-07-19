2472e09b50107457285e94467c1283603d81933e
amosnistrian <amosnistrian@gmail.com>
2017-07-18 19:14:25 -0700

    completed didPressNumber, didPressClear

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -23,7 +23,7 @@ enum modes {
 class ViewController: UIViewController {
 
     @IBOutlet weak var label: UILabel!
-    
+
     var labelString:String = "0"
     var currentMode:modes = .NO_MODE_SET
     var savedNum:Int = 0
@@ -33,23 +33,42 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
     @IBAction func didPressDecimalPoint(_ sender: Any) {
     }
+    
     @IBAction func didPressEquals(_ sender: Any) {
     }
+    
     @IBAction func didPressNegative(_ sender: Any) {
     }
+    
     @IBAction func didPressClear(_ sender: Any) {
+        // reset everything
+        label.text = "0"
+        labelString = "0"
+        currentMode = modes.NO_MODE_SET
+        savedNum = 0
+        lastButtonWasMode = false
+
     }
+    
     @IBAction func didPressDivide(_ sender: Any) {
+    
     }
     @IBAction func didPressMultiply(_ sender: Any) {
     }
+    
     @IBAction func didPressSubtract(_ sender: Any) {
     }
+    
     @IBAction func didPressPlus(_ sender: Any) {
     }
+    
     @IBAction func didPressNumber(_ sender: UIButton) {
+        let stringValue:String? = sender.titleLabel?.text
+        labelString = labelString.appending(stringValue!)
+        updateText()
     }
 
     override func didReceiveMemoryWarning() {
@@ -57,8 +76,13 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    func uodateText() {
-        // updated after: any number press, clear, equals, negative, decimal
+    // updated after: any number press, clear, equals, negative, decimal
+    func updateText() {
+        
+        guard let labelInt:Int = Int(labelString) else {
+            return
+        }
+        label.text = "\(labelInt)"
         
     }
     
